8610beda8c480e2d796fff03d5c00be9022e63b9
spartan221 <imorales@unal.edu.co>
2023-04-30 12:44:44 -0500

    added lib folder

---
+++ src/lib/producer.js
@@ -0,0 +1,29 @@
+const amqp = require('amqplib/callback_api');
+
+// connect to RabbitMQ server
+const rabbitMqUrl = process.env.RABBIT_MQ_URL || '127.0.0.1';
+
+export const sendMessageToRabbitMQ = (message) => {
+    amqp.connect(`amqp://${rabbitMqUrl}`, function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+            const queueName = process.env.RABBIT_MQ_QUEUE;
+            const msg = message;
+
+            channel.assertQueue(queueName, {
+                durable: false
+            });
+
+            channel.sendToQueue(queueName, Buffer.from(msg));
+            console.log(" [x] Sent %s", msg);
+        });
+        setTimeout(function () {
+            connection.close();
+        }, 500);
+    });
+};
