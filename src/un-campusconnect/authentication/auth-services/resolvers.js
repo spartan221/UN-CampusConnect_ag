9e146a2957f22031ed554ea2c6ebfd6ad2d6c2a8
spartan221 <imorales@unal.edu.co>
2023-04-29 21:35:39 -0500

    added rabbit MQ producer imp

--- src/un-campusconnect/authentication/auth-services/resolvers.js
+++ src/un-campusconnect/authentication/auth-services/resolvers.js
@@ -1,6 +1,7 @@
 import { generalRequest } from '../../../utilities';
 import { AUTH_MS_URL as url, AUTH_MS_PORT as port } from '../../../server';
 import { entryPoint } from './entryPoint';
+import { sendMessageToRabbitMQ } from '../../../lib/producer';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
 
@@ -14,11 +15,17 @@ const resolvers = {
 			generalRequest({ url: `${URL}/resend-email`, method: 'POST', body: { email } })
 	},
 	Mutation: {
-		signup: (_, { user: { username, email, password, role } }) =>
-			generalRequest({ url: `${URL}/signup`, method: 'POST', body: { username, email, password, role } })
+		signup: async (_, { user: { username, email, password, role } }) => {
+			const res = await generalRequest({ url: `${URL}/signup`, method: 'POST', body: { username, email, password, role } })
+			if ( res.hasOwnProperty('error') ) return res;
+			// Mandar el mensaje al rabbitMQ con el correo del usuario que se registró, solo ejecutar
+			// si la peticion anterior fue exitosa codigo 200.
+			sendMessageToRabbitMQ( email );
+			return res;
+		}
 		,
-		confirmEmail: (_, { token }) => 
-			generalRequest({ url: `${URL}/confirm-email/${token}`, method:'GET' })
+		confirmEmail: (_, { token }) =>
+			generalRequest({ url: `${URL}/confirm-email/${token}`, method: 'GET' })
 		,
 	}
 }
